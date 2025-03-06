4ead1daa431fa1712f7f9d87921e9c9fad5af060
Aace <aacejovi@gmail.com>
2025-03-06 16:05:55 +0100

    Html valid

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,11 @@
 import { useForm } from "react-hook-form";
+import { validateUserName, validatePassword } from "../utils/formValidate";
 
 export const Login = () => {
   const {
     register,
     handleSubmit,
-    formState: { error, isSubmitting },
+    formState: { errors, isSubmitting },
   } = useForm();
 
   return (
@@ -12,22 +13,43 @@ export const Login = () => {
       <div className="w-full md:max-w-[320px] mx-auto">
         <h1 className="font-bold text-3xl mb-6">Login to your Account</h1>
         <form className="space-y-4">
-          <label className="floating-label">
-            <span>UserName</span>
-            <input
-              type="text"
-              placeholder="Username"
-              className="input input-lg"
-            />
-          </label>
-          <label className="floating-label">
-            <span>PassWord</span>
-            <input
-              type="password"
-              placeholder="Password"
-              className="input input-lg"
-            />
-          </label>
+          <div>
+            <label className="floating-label">
+              <span>UserName</span>
+              <input
+                type="text"
+                placeholder="Username"
+                className="input input-lg"
+                id="username"
+                {...register("username", {
+                  validate: (value) => validateUserName(value),
+                })}
+              />
+            </label>
+            {errors.username && (
+              <span className="text-sm text-red-600">
+                {errors.username.message}
+              </span>
+            )}
+          </div>
+
+          <div>
+            <label className="floating-label">
+              <span>PassWord</span>
+              <input
+                type="password"
+                placeholder="Password"
+                className="input input-lg"
+                id="password"
+                {...register('password',{
+                  validate:(value)=> validatePassword(value)
+                })}
+              />
+            </label>
+            {errors.password && (
+              <span className="text-sm text-red-600">{errors.password.message}</span>
+            )}
+          </div>
           <button className="btn btn-secondary w-full">Continue</button>
         </form>
       </div>
