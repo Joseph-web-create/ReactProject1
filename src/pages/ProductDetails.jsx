151b1879f78862baf4f0548687fec94d1f7d231d
Aace <aacejovi@gmail.com>
2025-03-18 15:25:19 +0100

    Single page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -14,21 +14,65 @@ export default function ProductDetails() {
       setLoading(true);
       try {
         const res = await getaSingleProduct(productId);
-        console.log(res);
-        setData(res.data.product);
+        setData(res.data);
       } catch (error) {
-        console.log(error);
         setError(error.response.data.message);
       } finally {
-        setLoading(true);
+        setLoading(false);
       }
     };
     getSingleProduct();
   }, [productId]);
+
+  console.log(data);
+
   return (
     <div className="container mx-auto py-6 px-4">
       {error && <p className="text-center text-red-600">{error}</p>}
-      {loading ? <Spinner /> : <></>}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="lg:flex">
+            <div className="lg:w-[60%]">
+              {data?.images?.map((item, index) => (
+                <img
+                  key={index}
+                  src={item}
+                  className="w-full h-[400px] mb-4"
+                  alt="Product images"
+                />
+              ))}
+            </div>
+            <div className="lg:w-[40%]">
+              <p className="font-bold mb-4">{data?.category}</p>
+              <h1 className="text-3xl font-bold">{data?.title}</h1>
+              <p className="text-xl mt-2">{data?.price}</p>
+              <p>
+                Availability: <span>{data?.availabilityStatus}</span>
+              </p>
+              <button className="btn btn-primary btn-lg w-full mt-4">
+                Add To Cart
+              </button>
+              <div className="">
+                <div className="divider"></div>
+                <h1 className="font-bold text-lg">Description</h1>
+                <p>{data?.description}</p>
+                <p className="mt-4">
+                  <span>Shipping Information</span>
+                  {" "}
+                  {data?.shippingInformation}
+                </p>
+                <p className="mt-4">
+                  <span>Warranty Information</span>
+                  {" "}
+                  {data?.warrantyInformation}
+                </p>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
